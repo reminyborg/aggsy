09bf35ad9482a41d3abcdc360b66a8461c24cfcf
Remi Nyborg <remi@nyborg.priv.no>
2016-02-24 23:00:21 +0100

    Added _avg and _stdev

--- reducers.js
+++ reducers.js
@@ -18,9 +18,29 @@ function _max (prev, curr) {
   else return prev
 }
 
+// newAverage = average + ((value - average) / index)
+function _avg (d, curr) {
+  d.count++
+  d.value = d.value + ((curr - d.value) / d.count)
+  return d
+}
+_avg.initialValue = { value: 0, count: 0 }
+
+// newAverage = average + ((value - average) / index)
+function _stdev (d, curr) {
+  d.count++
+  d.average = d.average + ((curr - d.average) / d.count)
+  d.variance = d.variance + ((Math.pow(curr - d.average, 2) - d.variance) / d.count)
+  d.value = Math.sqrt(d.variance)
+  return d
+}
+_stdev.initialValue = { value: 0, variance: 0, average: 0, count: 0 }
+
 module.exports = {
   '_sum': _sum,
   '_count': _count,
   '_min': _min,
-  '_max': _max
+  '_max': _max,
+  '_avg': _avg,
+  '_stdev': _stdev
 }
